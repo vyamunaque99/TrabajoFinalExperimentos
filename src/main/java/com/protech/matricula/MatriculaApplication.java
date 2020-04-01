4271aab0c5bb2cf05b0aac15777d77ef68794df8
Víctor Yamunaqué <vyamunaque99@gmail.com>
2020-04-01 18:55:07 -0500

    Configuración inicial

---
+++ src/main/java/com/protech/matricula/MatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.protech.matricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaApplication.class, args);
+	}
+
+}
